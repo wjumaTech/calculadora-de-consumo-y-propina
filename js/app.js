688c91d378e4742f65b74860d431f99a9c5d6275
Wilson Juma Alcántara <wjuma.tech@gmail.com>
2023-10-14 14:36:16 -0400

    Validando formulario al crear la orden

--- js/app.js
+++ js/app.js
@@ -3,3 +3,22 @@ const cliente = {
   hora: '',
   pedido: []
 }
+
+const btnCrearOrden = document.querySelector('#crear-orden');
+btnCrearOrden.addEventListener('click', guardarCliente);
+
+
+function guardarCliente() {
+
+  const mesa = document.querySelector('#mesa').value;
+  const hora = document.querySelector('#hora').value;
+
+  const isCampoVacio = [mesa, hora].some(campo => campo === "");
+
+  if (isCampoVacio) {
+    console.log('Si, hay al menos un campo vacio');
+  } else {
+    console.log('Todos los campos estan llenos');
+  }
+
+}
